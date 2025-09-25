164d475d6c8aac8a8177d4f6108f8f247daa3fee
kuua0119 <219413026+kuua0119@users.noreply.github.com>
2025-09-25 09:43:05 +0000

    いいね追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image   
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
+  
   validates :title, presence: true
   validates :body, presence: true
 end
