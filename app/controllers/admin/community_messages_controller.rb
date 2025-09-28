b8b2f7346246c2716c4d1718ae56c24c4f2a2855
kuua0119 <219413026+kuua0119@users.noreply.github.com>
2025-09-28 12:21:31 +0000

    管理者コミュニティメッセージ管理者変更

--- app/controllers/admin/community_messages_controller.rb
+++ app/controllers/admin/community_messages_controller.rb
@@ -1,15 +1,2 @@
 class Admin::CommunityMessagesController < ApplicationController
-  belongs_to :user
-  belongs_to :community
-  has_many_attached :files
-
-  after_create_commit -> {
-    CommunityChannel.broadcast_to(community, render_message)
-  }
-
-  private
-
-  def render_message
-    ApplicationController.renderer.render(partial: "public/community_messages/message", locals: { message: self })
-  end
-end
+  
